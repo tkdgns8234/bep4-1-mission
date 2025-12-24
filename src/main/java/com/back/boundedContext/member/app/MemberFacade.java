eac84433639439e1f469f8d968de4cf20a2c07d1
tkdgns8234 <tkdgns82345@naver.com>
2025-12-24 11:07:08 +0900

    mission-0012

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -2,6 +2,7 @@ package com.back.boundedContext.member.app;
 
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.member.out.MemberRepository;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +17,7 @@ public class MemberFacade {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Member join(String username, String password, String nickname) {
+    public RsData<Member> join(String username, String password, String nickname) {
         return memberJoinUseCase.join(username, password, nickname);
     }
 
