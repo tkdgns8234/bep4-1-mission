20b31eebd9d2f9e26c90abdb0e34e27f881dbc98
tkdgns8234 <tkdgns82345@naver.com>
2025-12-31 01:35:14 +0900

    mission-0047

--- src/main/java/com/back/boundedContext/payout/in/PayoutDataInit.java
+++ src/main/java/com/back/boundedContext/payout/in/PayoutDataInit.java
@@ -3,6 +3,15 @@ package com.back.boundedContext.payout.in;
 import com.back.boundedContext.payout.app.PayoutFacade;
 import com.back.boundedContext.payout.domain.policy.PayoutPolicy;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.job.JobExecution;
+import org.springframework.batch.core.job.parameters.InvalidJobParametersException;
+import org.springframework.batch.core.job.parameters.JobParameters;
+import org.springframework.batch.core.job.parameters.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.launch.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.JobRestartException;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,20 +19,28 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 @Slf4j
 public class PayoutDataInit {
     private final PayoutDataInit self;
     private final PayoutFacade payoutFacade;
+    private final JobOperator jobOperator;
+    private final Job payoutCollectItemsJob;
 
     PayoutDataInit(
             @Lazy PayoutDataInit self,
-            PayoutFacade payoutFacade
+            PayoutFacade payoutFacade,
+            JobOperator jobOperator,
+            Job payoutCollectItemsJob
     ) {
         this.self = self;
         this.payoutFacade = payoutFacade;
+        this.jobOperator = jobOperator;
+        this.payoutCollectItemsJob = payoutCollectItemsJob;
     }
 
     @Bean
@@ -32,6 +49,7 @@ public class PayoutDataInit {
         return args -> {
             self.forceMakePayoutReadyCandidatesItems();
             self.collectPayoutItemsMore();
+            self.runCollectPayoutItemsBatchJob();
         };
     }
 
@@ -48,7 +66,28 @@ public class PayoutDataInit {
     @Transactional
     public void collectPayoutItemsMore() {
         payoutFacade.collectPayoutItemsMore(4);
-        payoutFacade.collectPayoutItemsMore(2);
-        payoutFacade.collectPayoutItemsMore(2);
+//        payoutFacade.collectPayoutItemsMore(2);
+//        payoutFacade.collectPayoutItemsMore(2);
+    }
+
+    public void runCollectPayoutItemsBatchJob() {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString(
+                        "runDate",
+                        LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
+                )
+                .toJobParameters();
+
+        try {
+            JobExecution execution = jobOperator.start(payoutCollectItemsJob, jobParameters);
+        } catch (JobInstanceAlreadyCompleteException e) {
+            log.error("Job instance already complete", e);
+        } catch (JobExecutionAlreadyRunningException e) {
+            log.error("Job execution already running", e);
+        } catch (InvalidJobParametersException e) {
+            log.error("Invalid job parameters", e);
+        } catch (JobRestartException e) {
+            log.error("job restart exception", e);
+        }
     }
 }
